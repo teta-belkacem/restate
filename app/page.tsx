eba4124e38b5eb80db9173b43b00886a867c4a95
belkacem-teta <belkacem.teta@gmail.com>
2025-05-05 00:11:31 +0100

    finished initial supabase setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import { createClient } from "@/utils/supabase/client";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const { data, error} = await supabase
+    .from("states")
+    .select("*");
+  
+  if (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error fetching data</div>;
+  }
+
   return (
-    <div className="ms-5">
-      مرحبا
+    <div>
+      <h1>Supabase Data</h1>
+      {data && data.length > 0 ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.id} - {item.name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No data found</p>
+      )}
     </div>
   );
 }
